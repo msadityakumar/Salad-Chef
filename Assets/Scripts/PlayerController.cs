997a822a70328a414aef49830f96b0bffd297804
msadityakumar <msadityakumar@gmail.com>
2019-04-08 00:11:30 +0530

    Added new scene with two player characters.
    Added Player controller.
    Added Cinemachine to track players movement in camera frustum.
    Added Placeholder images for players.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+
+    [SerializeField] private string InputHorizontalAxis;
+    [SerializeField] private string InputVerticalAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis(InputHorizontalAxis);
+        float yAxis = Input.GetAxis(InputVerticalAxis);
+
+        Vector3 movement = new Vector3(xAxis * Time.deltaTime * Speed, yAxis * Time.deltaTime * Speed, 0f);
+        this.gameObject.transform.Translate(movement);
+    }
+}
